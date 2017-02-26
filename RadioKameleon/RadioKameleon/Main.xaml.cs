ab0409633823f571c6c6281fa923dd15ef833ed4
Adin Halilovic <adin.halilovic@gmail.com>
2017-02-26 10:33:17 +0100

    Init commit

---
+++ RadioKameleon/RadioKameleon/Main.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace RadioKameleon
+{
+    public partial class Main : ContentPage
+    {
+        public Main()
+        {
+            InitializeComponent();
+            MyWebView.Source = new HtmlWebViewSource() {BaseUrl = "http://radiokameleon.ba/" };
+        }
+    }
+}
